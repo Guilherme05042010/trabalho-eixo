e6732b0093bbb9e53d7f0766016560e89e01ccb2
Guilherme05042010 <109686338+Guilherme05042010@users.noreply.github.com>
2022-09-08 18:54:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,259 @@
+function LeiaMais(){
+    var pontos=document.getElementById("pontos")
+    var maisTexto=document.getElementById("mais")
+    var btnLeiaMais=document.getElementById("btnLeiaMais")
+
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        btnLeiaMais.innerHTML="▼"
+    }else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnLeiaMais.innerHTML="▲"
+        
+    }
+}
+
+
+function LeiaMais2(){
+    var pontos2=document.getElementById("pontos2")
+    var maisTexto2=document.getElementById("mais2")
+    var btnLeiaMais2=document.getElementById("btnLeiaMais2")
+
+    if(pontos2.style.display === "none"){
+        pontos2.style.display="inline";
+        maisTexto2.style.display="none";
+        btnLeiaMais2.innerHTML="▼"
+    }else{
+        pontos2.style.display="none";
+        maisTexto2.style.display="inline";
+        btnLeiaMais2.innerHTML="▲"
+        
+    }
+}
+
+
+function LeiaMais3(){
+    var pontos3=document.getElementById("pontos3")
+    var maisTexto3=document.getElementById("mais3")
+    var btnLeiaMais3=document.getElementById("btnLeiaMais3")
+
+    if(pontos3.style.display === "none"){
+        pontos3.style.display="inline";
+        maisTexto3.style.display="none";
+        btnLeiaMais3.innerHTML="▼"
+    }else{
+        pontos3.style.display="none";
+        maisTexto3.style.display="inline";
+        btnLeiaMais3.innerHTML="▲"
+        
+    }
+}
+
+
+function LeiaMais4(){
+    var pontos4=document.getElementById("pontos4")
+    var maisTexto4=document.getElementById("mais4")
+    var btnLeiaMais4=document.getElementById("btnLeiaMais4")
+
+    if(pontos4.style.display === "none"){
+        pontos4.style.display="inline";
+        maisTexto4.style.display="none";
+        btnLeiaMais4.innerHTML="▼"
+    }else{
+        pontos4.style.display="none";
+        maisTexto4.style.display="inline";
+        btnLeiaMais4.innerHTML="▲"
+        
+    }
+}
+
+
+function LeiaMais5(){
+    var pontos5=document.getElementById("pontos5")
+    var maisTexto5=document.getElementById("mais5")
+    var btnLeiaMais5=document.getElementById("btnLeiaMais5")
+
+    if(pontos5.style.display === "none"){
+        pontos5.style.display="inline";
+        maisTexto5.style.display="none";
+        btnLeiaMais5.innerHTML="▼"
+    }else{
+        pontos5.style.display="none";
+        maisTexto5.style.display="inline";
+        btnLeiaMais5.innerHTML="▲"
+        
+    }
+}
+
+
+
+function LeiaMais6(){
+    var pontos6=document.getElementById("pontos6")
+    var maisTexto6=document.getElementById("mais6")
+    var btnLeiaMais6=document.getElementById("btnLeiaMais6")
+
+    if(pontos6.style.display === "none"){
+        pontos6.style.display="inline";
+        maisTexto6.style.display="none";
+        btnLeiaMais6.innerHTML="▼"
+    }else{
+        pontos6.style.display="none";
+        maisTexto6.style.display="inline";
+        btnLeiaMais6.innerHTML="▲"
+        
+    }
+}
+
+
+
+function LeiaMais7(){
+    var pontos7=document.getElementById("pontos7")
+    var maisTexto7=document.getElementById("mais7")
+    var btnLeiaMais7=document.getElementById("btnLeiaMais7")
+
+    if(pontos7.style.display === "none"){
+        pontos7.style.display="inline";
+        maisTexto7.style.display="none";
+        btnLeiaMais7.innerHTML="▼"
+    }else{
+        pontos7.style.display="none";
+        maisTexto7.style.display="inline";
+        btnLeiaMais7.innerHTML="▲"
+        
+    }
+}
+
+
+
+function LeiaMais8(){
+    var pontos8=document.getElementById("pontos8")
+    var maisTexto8=document.getElementById("mais8")
+    var btnLeiaMais8=document.getElementById("btnLeiaMais8")
+
+    if(pontos8.style.display === "none"){
+        pontos8.style.display="inline";
+        maisTexto8.style.display="none";
+        btnLeiaMais8.innerHTML="▼"
+    }else{
+        pontos8.style.display="none";
+        maisTexto8.style.display="inline";
+        btnLeiaMais8.innerHTML="▲"
+        
+    }
+}
+
+
+
+function LeiaMais9(){
+    var pontos9=document.getElementById("pontos9")
+    var maisTexto9=document.getElementById("mais9")
+    var btnLeiaMais9=document.getElementById("btnLeiaMais9")
+
+    if(pontos9.style.display === "none"){
+        pontos9.style.display="inline";
+        maisTexto9.style.display="none";
+        btnLeiaMais9.innerHTML="▼"
+    }else{
+        pontos9.style.display="none";
+        maisTexto9.style.display="inline";
+        btnLeiaMais9.innerHTML="▲"
+        
+    }
+}
+
+
+
+function LeiaMais10(){
+    var pontos10=document.getElementById("pontos10")
+    var maisTexto10=document.getElementById("mais10")
+    var btnLeiaMais10=document.getElementById("btnLeiaMais10")
+
+    if(pontos10.style.display === "none"){
+        pontos10.style.display="inline";
+        maisTexto10.style.display="none";
+        btnLeiaMais10.innerHTML="▼"
+    }else{
+        pontos10.style.display="none";
+        maisTexto10.style.display="inline";
+        btnLeiaMais10.innerHTML="▲"
+        
+    }
+}
+
+
+
+function LeiaMais11(){
+    var pontos11=document.getElementById("pontos11")
+    var maisTexto11=document.getElementById("mais11")
+    var btnLeiaMais11=document.getElementById("btnLeiaMais11")
+
+    if(pontos11.style.display === "none"){
+        pontos11.style.display="inline";
+        maisTexto11.style.display="none";
+        btnLeiaMais11.innerHTML="▼"
+    }else{
+        pontos11.style.display="none";
+        maisTexto11.style.display="inline";
+        btnLeiaMais11.innerHTML="▲"
+        
+    }
+}
+
+
+
+function LeiaMais12(){
+    var pontos12=document.getElementById("pontos12")
+    var maisTexto12=document.getElementById("mais12")
+    var btnLeiaMais12=document.getElementById("btnLeiaMais12")
+
+    if(pontos12.style.display === "none"){
+        pontos12.style.display="inline";
+        maisTexto12.style.display="none";
+        btnLeiaMais12.innerHTML="▼"
+    }else{
+        pontos12.style.display="none";
+        maisTexto12.style.display="inline";
+        btnLeiaMais12.innerHTML="▲"
+        
+    }
+}
+
+
+
+function LeiaMais13(){
+    var pontos13=document.getElementById("pontos13")
+    var maisTexto13=document.getElementById("mais13")
+    var btnLeiaMais13=document.getElementById("btnLeiaMais13")
+
+    if(pontos13.style.display === "none"){
+        pontos13.style.display="inline";
+        maisTexto13.style.display="none";
+        btnLeiaMais13.innerHTML="▼"
+    }else{
+        pontos13.style.display="none";
+        maisTexto13.style.display="inline";
+        btnLeiaMais13.innerHTML="▲"
+        
+    }
+}
+
+
+
+function LeiaMais14(){
+    var pontos14=document.getElementById("pontos14")
+    var maisTexto14=document.getElementById("mais14")
+    var btnLeiaMais14=document.getElementById("btnLeiaMais14")
+
+    if(pontos14.style.display === "none"){
+        pontos14.style.display="inline";
+        maisTexto14.style.display="none";
+        btnLeiaMais14.innerHTML="▼"
+    }else{
+        pontos14.style.display="none";
+        maisTexto14.style.display="inline";
+        btnLeiaMais14.innerHTML="▲"
+        
+    }
+}
